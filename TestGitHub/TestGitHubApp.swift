5bd9cc3e4df0021b846d217ea646a2a1bd4f2df9
myMacosGit <reg.isaacs@gmail.com>
2022-10-04 12:13:49 +0200

    Initial Commit

---
+++ TestGitHub/TestGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGitHubApp.swift
+//  TestGitHub
+//
+//  Created by Richard Isaacs on 04.10.22.
+//
+
+import SwiftUI
+
+@main
+struct TestGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
